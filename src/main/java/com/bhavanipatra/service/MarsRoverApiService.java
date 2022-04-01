0d485524ea7cb9b2d81e91825ea9be43f1b3a05e
Bhavani Shankar Patra <bhavanipatra@gmail.com>
2022-04-01 23:53:32 +0530

    Added functionality to select Mar's days i.e sol

--- src/main/java/com/bhavanipatra/service/MarsRoverApiService.java
+++ src/main/java/com/bhavanipatra/service/MarsRoverApiService.java
@@ -9,10 +9,13 @@ import com.bhavanipatra.response.MarsRoverApiResponse;
 @Service
 public class MarsRoverApiService {
 
-	public MarsRoverApiResponse getRoverData(String roverName) {
-        RestTemplate rt = new RestTemplate();
-		
-		ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/" + roverName + "/photos?sol=2&api_key=DEMO_KEY", MarsRoverApiResponse.class);
-		return response.getBody();
-	}
+  private static final String API_KEY = "f7HYj19tL9KJxtwtRPBTW7FXLDTiP1h9l6rxRy0O";
+  
+  public MarsRoverApiResponse getRoverData(String roverType, Integer marsSol) {
+    RestTemplate rt = new RestTemplate();
+    
+    ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverType+"/photos?sol="+marsSol+"&api_key=" + API_KEY, MarsRoverApiResponse.class);
+    
+    return response.getBody();
+  }
 }
